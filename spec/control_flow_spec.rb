517b78330575167b45e35e02bc2ac44c7f252572
Alexander Obregon <alexander.obregon47@gmail.com>
2022-01-31 10:38:52 -0800

    commit tree a77780e0ba19dbf2617f47b212b681a59a509d9b

--- spec/control_flow_spec.rb
+++ spec/control_flow_spec.rb
@@ -1,5 +1,19 @@
+require 'pry'
+
 require_relative 'spec_helper'
 require_relative '../control_flow'
+
+def admin_login(username, password)
+  if username == 'admin' && password == '12345'
+    "Access granted"
+  elsif username == 'ADMIN' && password == '12345'
+    "Access granted"
+  else
+    
+    "Access denied"
+
+  end
+end
 
 describe '#admin_login' do
   
@@ -18,6 +32,20 @@ describe '#admin_login' do
   end
 
 end
+
+
+  def hows_the_weather(temperature)
+    if temperature < 40
+      "It's brisk out there!"
+    elsif temperature >= 40 && temperature <= 65
+      "It's a little chilly out there!"
+    elsif temperature > 85
+      "It's too dang hot out there!"
+    else 
+      "It's perfect out there!"
+    end 
+  end
+
 
 describe '#hows_the_weather' do
   
@@ -38,6 +66,23 @@ describe '#hows_the_weather' do
   end
 
 end
+
+
+def fizzbuzz(num)
+if num % 3 == 0 && num % 5 == 0
+  "FizzBuzz"
+elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+      "Buzz"
+  else
+    num
+  end
+end
+
+
+
+
 
 describe '#fizzbuzz' do
   
@@ -62,6 +107,26 @@ describe '#fizzbuzz' do
   end
 
 end
+
+def calculator(operation, num1, num2)
+  
+  case operation
+  when "+"
+     num1 + num2
+  when "-"
+    num1 - num2
+  when "*"
+    num1 * num2
+  when "/"
+    num1 / num2
+  else
+   puts  "Invalid operation!"
+  end
+
+end
+
+
+
 
 describe '#calculator' do
   
